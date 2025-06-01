b53935a10f809af4a13cd32e3db12f6878f6d5ab
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2025-06-01 12:31:09 +0300

    added img

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -39,17 +39,17 @@
                 <h3 class="fw-bold text-success mb-4">شركاء النجاح</h3>
                 <div class="row justify-content-center g-4">
                     <div class="col-6 col-md-4 col-lg-3">
-                        <img src="{{ asset('assets/images/egypt-post.png') }}" class="img-fluid rounded shadow-sm"
+                        <img src="{{ asset('assets/images/egypt-post.jpg') }}" class="img-fluid rounded shadow-sm"
                             alt="البريد المصري">
                     </div>
-                    <div class="col-6 col-md-4 col-lg-3">
+                    {{-- <div class="col-6 col-md-4 col-lg-3">
                         <img src="{{ asset('assets/images/partner-a.png') }}" class="img-fluid rounded shadow-sm"
                             alt="شريك A&M">
                     </div>
                     <div class="col-6 col-md-4 col-lg-3">
                         <img src="{{ asset('assets/images/partner-ngo.png') }}" class="img-fluid rounded shadow-sm"
                             alt="منظمة حقوق الإنسان">
-                    </div>
+                    </div> --}}
                 </div>
             </div>
 
